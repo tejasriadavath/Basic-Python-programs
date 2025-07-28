580d56c1ff73dc35aec9338fa61e944ce726ae16
tejasriadavath <148548732+tejasriadavath@users.noreply.github.com>
2025-07-28 21:06:04 +0530

    Add files via upload
    Program to check whether a number is even or odd.

---
+++ even or odd.py
@@ -0,0 +1,12 @@
+number = int(input("Enter a number: ")) # getting a number input to check for odd or even 
+
+# checking number using the remainder
+
+if number % 2 == 0:
+
+print("The number is Even") # Even no if remainder is 0
+
+else:
+
+print("The number is Odd") # Odd no if remainder is not 0
+
